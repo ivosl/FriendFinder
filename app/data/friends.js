c7245a129d26f6612e56f77797961c76aca3d7fe
Ivo <ivol@Ivos-MacBook-Air.local>
2018-03-08 17:32:28 -0600

    updating files

---
+++ app/data/friends.js
@@ -0,0 +1,49 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the waitlist tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friendsArray = [
+    {
+        name:"Ivo",
+        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores:[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+    }
+  ];
+  
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
+
+  var friendsArray = [];
+
+  function createUser(newUser) {
+      var name = newUser.name;
+      var photo = newUser.photo;
+      var scores = 
+
+      function UserInfo(name, photo, scores) {
+          this.name = name;
+          this.photo = photo;
+          this.scores = scores;
+      }
+      var newUser = new userInfo(name, photo, scores);
+
+      if (compare scores.apply.)
+  }
+
+ 
+  
